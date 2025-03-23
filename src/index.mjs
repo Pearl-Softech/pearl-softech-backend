1e44d0efef7a70ad51e83945b6f8c6f11a71fb27
devRajeshThapa <devrajeshthapa@gmail.com>
2025-03-23 14:02:18 +0545

    CORS

--- src/index.mjs
+++ src/index.mjs
@@ -12,9 +12,14 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-API-Key'], // Allow specific headers, including X-API-Key
+}));
+
 app.use("/", router);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server started listening at PORT ${PORT}`)
 });
