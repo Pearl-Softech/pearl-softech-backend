1371cfe26ecce717dca46aa99b3ed4a9d99040b7
devRajeshThapa <devrajeshthapa@gmail.com>
2025-03-23 14:18:38 +0545

    CORS Related

--- src/index.mjs
+++ src/index.mjs
@@ -1,26 +1,30 @@
 import express from "express";
-import "dotenv/config"
+import "dotenv/config";
 import cors from "cors";
 
 import router from "./routes/index.mjs";
 import { connectMongoDB } from "./connections/index.mjs";
 
 const PORT = process.env.PORT || 8080;
-connectMongoDB("mongodb://127.0.0.1:27017/pearl-softech")
+connectMongoDB("mongodb://127.0.0.1:27017/pearl-softech");
 
 const app = express();
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json());
-app.options('*', cors()); // Enable CORS preflight for all routes
+
+// Enable CORS for all routes (move to top)
 app.use(cors({
     origin: '*', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-API-Key'], // Allow specific headers, including X-API-Key
 }));
 
+// Allow preflight OPTIONS request handling for all routes
+app.options('*', cors()); 
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use("/", router);
 
 app.listen(PORT, () => {
-    console.log(`Server started listening at PORT ${PORT}`)
+    console.log(`Server started listening at PORT ${PORT}`);
 });
