05b0e73d9ed2422028aef78dc8d90873899fcec3
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-04-20 19:36:29 +0545

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -10,6 +10,12 @@ connectMongoDB("mongodb://127.0.0.1:27017/pearl-softech");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+
 // Enable CORS for all routes (move to top)
 app.use(cors({
     origin: ['https://pearlsoftech.com', 'https://admin.pearlsoftech.com'],
@@ -20,9 +26,13 @@ app.use(cors({
 // Allow preflight OPTIONS request handling for all routes
 app.options('*', cors());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
 app.use("/", router);
 
 app.listen(PORT, () => {
