48fd302bd762e8e4cde0ffa8a8b2536c84e8477d
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-04-24 00:57:13 +0545

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -25,8 +25,8 @@ app.options('*', (req, res) => {
   res.sendStatus(200);
 });
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use("/", router);
 
 app.listen(PORT, () => {
